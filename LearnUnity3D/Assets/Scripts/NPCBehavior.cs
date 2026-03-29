6d6a94c80afcd82ee80b240747722bfcae93e884
NNNNateLiu <sl8292@nyu.edu>
2026-03-29 21:34:07 +0800

    Update Level Navmesh & Add AI NPC logic

---
+++ LearnUnity3D/Assets/Scripts/NPCBehavior.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.AI;
+using UnityEngine.AI;
+
+public class NPCBehavior : MonoBehaviour
+{
+    public GameObject tagetPosition;
+
+    private void Update()
+    {
+        Invoke("MovetoTarget",4f);
+
+        if (gameObject.GetComponent<NavMeshAgent>().velocity.magnitude > 0)
+        {
+            gameObject.GetComponent<Animator>().SetBool("isWalking",true);
+        }
+        else
+        {
+            gameObject.GetComponent<Animator>().SetBool("isWalking",false);
+        }
+    }
+
+    private void MovetoTarget()
+    {
+        gameObject.GetComponent<NavMeshAgent>().SetDestination(tagetPosition.transform.position);
+    }
+}
